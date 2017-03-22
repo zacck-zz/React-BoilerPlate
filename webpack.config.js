ac4859bfc5f1c87f0b06182f3c1c707664181a87
zacck <coderv63@gmail.com>
2017-03-22 11:26:13 +0200

    work on plugins config on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,109 @@
 var webpack = require('webpack');
 var path = require('path');
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var CompressionPlugin = require("compression-webpack-plugin");
+var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+
+//enviroment variable
+var DEVELOPMENT = process.env.NODE_ENV === 'development';
+var PRODUCTION = process.env.NODE_ENV === 'production';
+var TEST = process.env.NODE_ENV === 'test'
+
+//check if on production
+var entry = PRODUCTION
+    ?   {
+          app:
+           [
+             './app.jsx',
+           ],
+          vendor:
+          [
+            'script-loader!jquery/dist/jquery.min.js',
+            'script-loader!foundation-sites/dist/foundation.min.js',
+            'react',
+            'redux',
+            'react-redux',
+            'react-router',
+            'firebase',
+            'react-dom',
+            'redux-thunk'
+          ]
+        }
+    :   [
+
+          './app.jsx',
+          'script-loader!jquery/dist/jquery.min.js',
+          'script-loader!foundation-sites/dist/foundation.min.js',
+        ];
+
+var plugins = PRODUCTION
+    ?     [
+            new webpack.optimize.CommonsChunkPlugin({name:'vendor', filename:'vendor.[hash:12].min.js'}),
+            new webpack.optimize.UglifyJsPlugin({
+              compress: {
+                warnings: false,
+                screw_ie8: true,
+                dead_code: true,
+                unused: true
+              }
+            }),
+            new HTMLWebpackPlugin({
+              template:'index-template.html'
+            }),
+            new CompressionPlugin({
+              asset: "[path].gz[query]",
+              algorithm: "gzip",
+              test: /\.js$|\.css$|\.html$/,
+              threshold: 10240,
+              minRatio: 0.8
+            }),
+            new SWPrecacheWebpackPlugin(
+              {
+                cacheId: 'hapihour',
+                filename: 'hapi-sw.js',
+                forceDelete: true,
+                minify: true,
+                skipWaiting: true,
+                verbose: true
+              }
+            )
+          ]
+    :     [
+            new webpack.NamedModulesPlugin()
+          ];
+
+
+//ADD UNIVERSAL PLUGINS
+plugins.push(
+  new webpack.DefinePlugin({
+   'process.env': {
+     NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+     API_KEY: JSON.stringify(process.env.API_KEY),
+     AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+     DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+     STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
+     MESSAGE_SENDER_ID: JSON.stringify(process.env.MESSAGE_SENDER_ID)
+   }
+ }),
+ new webpack.LoaderOptionsPlugin({
+      minimize:true,
+      debug: false,
+      test: /\.scss$/,
+      options: {
+        sassLoader: {
+          includePaths: [
+            path.resolve(__dirname, './node_modules/foundation-sites/scss')
+          ]
+        }
+      }
+  })
+);
+
 
 module.exports =  {
   //find this file and start from there
-  entry: [
-    'script!jquery/dist/jquery.min.js',
-    'script!foundation-sites/dist/foundation.min.js',
-    './app/app.jsx'
-  ],
+  context: __dirname + '/app',
+  entry:entry,
   externals: {
     jquery: 'jQuery'
   },
