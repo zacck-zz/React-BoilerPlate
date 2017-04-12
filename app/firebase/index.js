b16657468be84c48bda29b300a33c994e162e67c
zacck <coderv63@gmail.com>
2017-04-12 12:11:12 +0200

    work on adding firebase to the project for cloud function samples

---
+++ app/firebase/index.js
@@ -0,0 +1,19 @@
+
+import firebase from 'firebase';
+//use try since async
+try {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC87ODgKYf8LzVuAs5N_bA1eE-7KyGHun0",
+    authDomain: "gentest-7b8cd.firebaseapp.com",
+    databaseURL: "https://gentest-7b8cd.firebaseio.com",
+    storageBucket: "gentest-7b8cd.appspot.com",
+  };
+  firebase.initializeApp(config);
+} catch(e) {
+
+}
+
+//get database root reference
+export var firebaseRef = firebase.database().ref();
+export default firebase;
